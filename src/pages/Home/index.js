86d6294319735a944f1527d067f325a3a09e4ee9
fabio <fabiosjordao@gmail.com>
2020-04-29 00:03:29 -0300

    Estilizando lista de produtos na home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,89 @@
 import React from 'react';
-
-import { Title } from './styles';
+import imagemIlustrativa from '../../assets/images/monitor-ilustrativo.jpg';
+import { ProductList } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
 
 export default function Main() {
-  return <Title>Main</Title>;
+  return (
+    <ProductList>
+      <li>
+        <img src={imagemIlustrativa} alt="Produto Ilustrativo" />
+        <strong>Produto Ilustrativo</strong>
+        <span>R$ 5.199,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color={'#FFF'} /> 2
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img src={imagemIlustrativa} alt="Produto Ilustrativo" />
+        <strong>Produto Ilustrativo</strong>
+        <span>R$ 5.199,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color={'#FFF'} /> 2
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img src={imagemIlustrativa} alt="Produto Ilustrativo" />
+        <strong>Produto Ilustrativo</strong>
+        <span>R$ 5.199,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color={'#FFF'} /> 2
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img src={imagemIlustrativa} alt="Produto Ilustrativo" />
+        <strong>Produto Ilustrativo</strong>
+        <span>R$ 5.199,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color={'#FFF'} /> 2
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img src={imagemIlustrativa} alt="Produto Ilustrativo" />
+        <strong>Produto Ilustrativo</strong>
+        <span>R$ 5.199,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color={'#FFF'} /> 2
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img src={imagemIlustrativa} alt="Produto Ilustrativo" />
+        <strong>Produto Ilustrativo</strong>
+        <span>R$ 5.199,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color={'#FFF'} /> 2
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,68 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
+export const ProductList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+  list-style: none;
 
-export const Title = styled.h1`
-  color: #555;
+  li {
+    display: flex;
+    flex-direction: column;
+    background: #fff;
+    border: solid 1px #eee;
+    border-radius: 3px;
+    padding: 20px;
+
+    img {
+      align-self: center;
+      max-width: 250px;
+    }
+
+    > strong {
+      font-size: 16px;
+      color: #333;
+      margin-top: 5px;
+    }
+
+    > span {
+      font-size: 21px;
+      font-weight: bold;
+      margin: 5px 0 20px;
+    }
+
+    button {
+      background: #e77f38;
+      color: #fff;
+      border-radius: 3px;
+      border: 0;
+      overflow: hidden;
+      margin-top: auto;
+
+      display: flex;
+      align-items: center;
+
+      &:hover {
+        background: ${darken(0.08, '#e77f38')};
+      }
+
+      div {
+        display: flex;
+        align-items: center;
+        padding: 12px;
+        background: rgba(0, 0, 8, 0.1);
+
+        svg {
+          margin-right: 5px;
+        }
+      }
+
+      span {
+        flex: 1;
+        text-align: center;
+        font-weight: bold;
+        text-transform: uppercase;
+      }
+    }
+  }
 `;
