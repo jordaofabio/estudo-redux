f0ac3cb34f77b93bf25ef7b09b48a95b94c4da2a
fabio <fabiosjordao@gmail.com>
2020-04-28 22:16:29 -0300

    Start

---
+++ src/styles/globals.js
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+
+* {
+  margin: 0;
+  padding: 0;
+  outline: 0;
+  box-sizing: border-box;
+}
+
+#root {
+  max-width: 1020px;
+  min-height: 100%;
+  margin: 0 auto;
+  padding: 0 20px 50px;
+}
+
+body {
+  background: #FFF;
+  color:#333;
+  -webkit-font-smoothing: antialiased !important;
+}
+
+body, input, button {
+  color: #222;
+  font-size: 14px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+button {
+  cursor: pointer;
+}
+`;
