44caaaa87e5e9dc7b25b1356b4d0cc1a1f3588fb
fabio <fabiosjordao@gmail.com>
2020-05-03 16:53:43 -0300

    Atualizando a quantidade de produtos repetidos

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,7 +1,16 @@
 export default function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [...state, action.product];
+      const existingProduct = state.findIndex(
+        (p) => p.id === action.product.id
+      );
+      if (existingProduct >= 0) {
+        state[existingProduct].amount = state[existingProduct].amount + 1;
+        return [...state];
+      } else {
+        return [...state, { ...action.product, amount: 1 }];
+      }
+
     default:
       return state;
   }
