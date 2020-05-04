6d74ef47175500b47cd548207fd14e6e7597c5da
fabio <fabiosjordao@gmail.com>
2020-05-03 22:47:37 -0300

    Reescrevendo cart reducer com Immer

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,16 +1,18 @@
+import produce from 'immer';
+
 export default function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
-      const existingProduct = state.findIndex(
-        (p) => p.id === action.product.id
-      );
-      if (existingProduct >= 0) {
-        state[existingProduct].amount = state[existingProduct].amount + 1;
-        return [...state];
-      } else {
-        return [...state, { ...action.product, amount: 1 }];
-      }
-
+      return produce(state, (draft) => {
+        const existingProduct = draft.findIndex(
+          (product) => product.id === action.product.id
+        );
+        if (existingProduct >= 0) {
+          draft[existingProduct].amount += 1;
+        } else {
+          draft.push({ ...action.product, amount: 1 });
+        }
+      });
     default:
       return state;
   }
